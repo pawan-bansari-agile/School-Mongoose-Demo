f913adbbc1b32ad812fcc99e6440aa616fc6c3a9
pawan-bansari-agile <pawan.bansari@agileinfoways.com>
2023-03-21 12:21:36 +0530

    changes in student module

--- src/web/students/students.module.ts
+++ src/web/students/students.module.ts
@@ -3,19 +3,10 @@ import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
 import { Student, StudentSchema } from 'src/schemas/students.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async () => ({
-        secret: process.env.JWT_SECRET,
-      }),
-      inject: [ConfigService],
-    }),
   ],
   controllers: [StudentsController],
   providers: [StudentsService],
